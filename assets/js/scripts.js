b254ebaa9dd8330f5bd73650fe378591fdc54d5e
Logan D <logandufek@gmail.com>
2021-07-01 22:46:24 -0400

    Working Javascript that currently returns the web adress for the posters of the movies shared between two actors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,193 +1,159 @@
-// var movieTest = fetch(
-//     "http://www.omdbapi.com/?a=ChrisPratt&apikey=d86e2804"
-// )
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (response) {
-//     console.log(response)
-// });
 
+
+//Initial set of arrays, to be added to later
 var filmArray1 = [];
 var filmArray2 = [];
 var finalResults = [];
 
-function checkActors(){
-userResponse1 = "Ben Affleck"
-userResponse2 = "Matt Damon"
+//checkActors function which will eventually be triggered by a users button press
+function checkActors() {
+    //user responses, set as defined strings currently but will be set to take in values based on what the user types in
+    userResponse1 = "Ben Affleck"
+    userResponse2 = "Matt Damon"
 
-fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse1 +"/" , {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-		"x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-	}
-
-
-
-})
-.then(function (response) {
-    return response.json();
-})
-.then(function (response) {
-    var ActorID = response.Actors[0].imdb_id
-    console.log(response)
-
-
-    fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
+    //inital fetch request for the first actor, retrieves Imdb ID data to then be used 
+    fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse1 + "/", {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-            "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
+            "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
         }
 
-        
+
+
     })
-    .then(response => {
-        return response.json();
-    })
-    .then(function (data) {
-        for (i = 0; i < data.Roles.movies.length; i++){
-        filmArray1.push(data.Roles.movies[i][0].title)
-        
-    }
-    })
-
-    .catch(err => {
-        console.error(err);
-    });
-
-})
-.catch(err => {
-	console.error(err);
-    
-});
+        .then(function (response) {
+            return response.json();
+        })
+        //taking that initial fetch, logging data as a variable and then passing it to the next fetch request
+        .then(function (response) {
+            var ActorID = response.Actors[0].imdb_id
+            console.log(response)
 
 
-fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse2 +"/" , {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-		"x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-	}
-})
-.then(function (response) {
-    return response.json();
-})
-.then(function (response) {
-    var ActorID = response.Actors[0].imdb_id
-    console.log(response)
+            fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
+                    "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
+                }
 
 
-    fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
+            })
+                .then(response => {
+                    return response.json();
+                })
+                //build an array from the desired data for the first actor, to be checked with the next array
+                .then(function (data) {
+                    for (i = 0; i < data.Roles.movies.length; i++) {
+                        if (filmArray1.includes(data.Roles.movies[i][0].title) === false) {
+                            filmArray1.push(data.Roles.movies[i][0].title)
+                        }
+                    }
+                    console.log(filmArray1)
+                })
+
+                .catch(err => {
+                    console.error(err);
+                });
+
+        })
+        .catch(err => {
+            console.error(err);
+
+        });
+
+    //Second series of fetch requests for the second entered actor
+    fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse2 + "/", {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-            "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
+            "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
         }
-
-        
     })
-    .then(response => {
-        return response.json();
-    })
-    .then(function (response) {
-        for (i = 0; i < response.Roles.movies.length; i++){
-            
-        filmArray2.push(response.Roles.movies[i][0].title)
-       
-        
-        
-        if (filmArray1.includes(filmArray2[i])){
-            console.log( filmArray2[i])
-        }
-        else {
-            console.log("no match")
-        }
-        }
+        .then(function (response) {
+            return response.json();
+        })
 
-    
-    })
-    
+        //same as the previous fetch request where we store the relevant data to then be used
+        .then(function (response) {
+            var ActorID = response.Actors[0].imdb_id
+            console.log(response)
 
 
-    .catch(err => {
-        console.error(err);
-    });
+            fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
+                    "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
+                }
 
-})
-.catch(err => {
-	console.error(err);
-    
-});
-console.log(filmArray1)
-console.log(filmArray2)
+
+            })
+                .then(response => {
+                    return response.json();
+                })
+                //build an array from the desired data for the second actor, to be checked with the first array
+                .then(function (response) {
+                    for (var i = 0; i < response.Roles.movies.length; i++) {
+                        if (filmArray2.includes(response.Roles.movies[i][0].title) === false) {
+                            filmArray2.push(response.Roles.movies[i][0].title)
+                            console.log(filmArray2)
+
+                        }
+
+
+                    }
+                    //pushes the films the two actors have in common to a new array
+                    for (var j = 0; j < filmArray2.length; j++) {
+
+                        if (filmArray1.includes(filmArray2[j])) {
+                            finalResults.push(filmArray2[j])
+
+
+
+                        }
+                        else {
+                            console.log("no match")
+                        }
+                    }
+
+                    console.log(finalResults)
+
+                    //for loop to iterate through this created array and retrieve the poster data from a new API
+                    for (i = 0; i < finalResults.length; i++) {
+
+                        matchedMovie = finalResults[i]
+                        var movieTest = fetch(
+                            "http://www.omdbapi.com/?s=" + matchedMovie + "&apikey=d86e2804"
+                        )
+                            .then(function (response) {
+                                return response.json();
+                            })
+                            .then(function (response) {
+                                var poster = response.Search[0].Poster
+                                console.log(poster)
+                            });
+                    };
+
+                })
+
+
+
+                .catch(err => {
+                    console.error(err);
+                });
+
+        })
+        .catch(err => {
+            console.error(err);
+
+        })
+
+    console.log(filmArray1)
+    console.log(filmArray2)
 
 
 }
 
 checkActors();
-
-function checkActors2(){
-userResponse1 = "Tom Hanks"
-userResponse2 = "Chris Pratt"
-
-Promise.all([
-	fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse1 +"/" , {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-            "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-        }
-    }),
-	fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/" + userResponse2 +"/" , {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-            "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-        }
-    })
-]).then(function (responses) {
-	// Get a JSON object from each of the responses
-	return Promise.all(responses.map(function (response) {
-		return response.json();
-	}));
-}).then(function (data) {
-	// Log the data to the console
-	// You would do something with both sets of data here
-	console.log(data);
-
-    var ActorID = Promise.all(data.Array[0].Actors[0].imdb_id)
-    console.log(ActorID)
-
-
-    fetch("https://data-imdb1.p.rapidapi.com/actor/id/" + ActorID + "/all_roles/", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "1d90df3750mshd40006f295ac708p1a17dajsn87556815a8fb",
-            "x-rapidapi-host": "data-imdb1.p.rapidapi.com"
-        }
-
-        
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then(function (response) {
-        for (i = 0; i < response.Roles.movies.length; i++){
-        filmArray = (response.Roles.movies[i][0].title)
-        console.log(filmArray)
-    }
-    })
-
-    
-}).catch(function (error) {
-	// if there's an error, log it
-	console.log(error);
-});
-
-
-
-    }
-
-    // checkActors2();
